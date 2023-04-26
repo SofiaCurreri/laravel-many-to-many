428bb3869b0125ca7b94d20245e96c660bc7fc36
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-26 23:47:32 +0200

    added types

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -17,12 +18,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker) //dependency injection
     {
+        //la collection mi diventa un array normale con gli id delle categorie grazie a pluck()
+        $types = Type::all()->pluck('id');
+
         for($i = 0; $i < 40; $i++) {
             $project = new Project;
+            $project->type_id = $faker->randomElement($types);
             $project->title = $faker->catchPhrase();
             $project->slug = Str::of($project->title)->slug('-');
             // $project->image= $faker->imageUrl(640, 480, 'animals', true);
-            $project->text = $faker->paragraph(15);
+            $project->text = $faker->paragraph(35);
             $project->save();
         }
     }
