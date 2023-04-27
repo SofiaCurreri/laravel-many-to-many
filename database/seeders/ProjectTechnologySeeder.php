00eb08c62a59108bdc095044d5726166a7fe6a02
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-27 16:03:10 +0200

    avoided duplication

--- database/seeders/ProjectTechnologySeeder.php
+++ database/seeders/ProjectTechnologySeeder.php
@@ -17,13 +17,13 @@ class ProjectTechnologySeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $projects = Project::all()->pluck('id'); //[1,2,...,40]
+        //$projects = Project::all()->pluck('id'); //[1,2,...,40]
         $technologies = Technology::all()->pluck('id'); //[1,2,...,7]
 
         //Per 40 volte ho preso un project randomico e gli ho attach una technology randomica
-        for($i = 0; $i < 40; $i++) {
-            $project = Project::find($faker->randomElement($projects));
-            $project->technologies()->attach($faker->randomElement($technologies));
+        for($i = 1; $i < 40; $i++) {
+            $project = Project::find($i);
+            $project->technologies()->attach($faker->randomElements($technologies, 3));
         }
     }
 }
