309e5c2d90f76984bc8d5ccb251fdaeb17a4c1df
SofiaCurreri <sofiacurreri@gmail.com>
2023-04-27 09:26:39 +0200

    pivot table seeder

---
+++ database/seeders/ProjectTechnologySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Technology;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectTechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $projects = Project::all()->pluck('id'); //[1,2,...,40]
+        $technologies = Technology::all()->pluck('id'); //[1,2,...,7]
+
+        //Per 40 volte ho preso un project randomico e gli ho attach una technology randomica
+        for($i = 0; $i < 40; $i++) {
+            $project = Project::find($faker->randomElement($projects));
+            $project->technologies()->attach($faker->randomElement($technologies));
+        }
+    }
+}
